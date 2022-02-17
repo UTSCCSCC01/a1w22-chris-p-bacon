8eacf3219a1a8e1507573ee4bf45b9aa464bfb31
Harshvardhan Gajabi <harsh.gajabi@mail.utoronto.ca>
2022-02-17 00:48:43 -0500

    Create context and start server

--- src/main/java/ca/utoronto/utm/mcs/App.java
+++ src/main/java/ca/utoronto/utm/mcs/App.java
@@ -1,20 +1,20 @@
 package ca.utoronto.utm.mcs;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import java.io.IOException;
 
 public class App
 {
     static int port = 8080;
 
-    public static void main(String[] args) throws IOException
-    {
-        // TODO Create Your Server Context Here, There Should Only Be One Context
-        System.out.printf("Server started on port %d\n", port);
+    public static void main(String[] args) throws IOException {
+        // Create Your Server Context Here, There Should Only Be One Context
+        Server server = DaggerServerComponent.create().buildServer();
+        ReqHandler reqHandler = DaggerReqHandlerComponent.create().buildHandler();
+        server.server.createContext("/api/v1", reqHandler);
 
-        // This code is used to get the neo4j address, you must use this so that we can mark :)
-        Dotenv dotenv = Dotenv.load();
-        String addr = dotenv.get("NEO4J_ADDR");
-        System.out.println(addr);
+        server.server.start();
+
+//        reqHandler.neo4jDAO.insertPokemon("Michael", "Humanbeing", "Fire", "Ice", "Old");
+        System.out.printf("Server started on port %d\n", port);
     }
 }
