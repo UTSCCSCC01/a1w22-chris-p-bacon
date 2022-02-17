1a9f38c7b91241d808e78826fd646c46cf4861b7
Harshvardhan Gajabi <harsh.gajabi@mail.utoronto.ca>
2022-02-17 12:58:47 -0500

    fix DI

--- src/main/java/ca/utoronto/utm/mcs/Neo4jDAO.java
+++ src/main/java/ca/utoronto/utm/mcs/Neo4jDAO.java
@@ -9,60 +9,43 @@ import javax.inject.Singleton;
 // go in this class
 @Singleton
 public class Neo4jDAO {
-    // TODO Complete This Class
+    public Driver driver;
 
-//    public Driver driver;
-//
-//    @Inject
-//    public Neo4jDAO(Driver driver) {
-//        this.driver = driver;
-//    }
-
-    private final Session session;
-    private final Driver driver;
-
-    private final String uriDb = "bolt://localhost:8080";
-    private final String username = "neo4j";
-    private final String password = "123456";
-
-
-    public Neo4jDAO() {
-        this.driver = GraphDatabase.driver(this.uriDb, AuthTokens.basic(this.username, this.password));
-        this.session = this.driver.session();
+    @Inject
+    public Neo4jDAO(Driver driver) {
+        this.driver = driver;
     }
 
     public void insertActor(String id, String name) {
         String query;
         query = "CREATE (n:Actor {id: \"%s\", name: \"%s\"})";
         query = String.format(query, id, name);
-        this.session.run(query);
-        return;
+        this.driver.session().run(query);
+    }
+
+    public void insertMovie(String id, String name) {
+        String query;
+        query = "CREATE (n:Movie {id: \"%s\", name: \"%s\"})";
+        query = String.format(query, id, name);
+        this.driver.session().run(query);
     }
 
     public void deleteActor(String id, String name) {
         String query;
         query = "MATCH (n:Actor {id: \"%s\", name: \"%s\"})";
         query = String.format(query, id, name);
-        this.session.run(query);
+        this.driver.session().run(query);
         query = "Delete n";
-        this.session.run(query);
-        return;
+        this.driver.session().run(query);
     }
-    public void insertMovie(String id, String name) {
-        String query;
-        query = "CREATE (n:Movie {id: \"%s\", name: \"%s\"})";
-        query = String.format(query, id, name);
-        this.session.run(query);
-        return;
-    }
+
 
     public void deleteMovie(String id, String name) {
         String query;
         query = "MATCH (n:Movie {id: \"%s\", name: \"%s\"})";
         query = String.format(query, id, name);
-        this.session.run(query);
+        this.driver.session().run(query);
         query = "Delete n";
-        this.session.run(query);
-        return;
+        this.driver.session().run(query);
     }
 }
