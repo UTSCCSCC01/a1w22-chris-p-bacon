36b5daf5cdcb01314138b9b7515a4bbd21384ca7
Harshvardhan Gajabi <harsh.gajabi@mail.utoronto.ca>
2022-02-18 14:53:06 -0500

    Changed DI structure

--- src/main/java/ca/utoronto/utm/mcs/Neo4jDAOComponent.java
+++
@@ -1,9 +0,0 @@
-package ca.utoronto.utm.mcs;
-import dagger.Component;
-import javax.inject.Singleton;
-
-@Singleton
-@Component(modules = Neo4jDAOModule.class)
-public interface Neo4jDAOComponent {
-    public Neo4jDAO buildNeo4jDAO();
-}
--- src/main/java/ca/utoronto/utm/mcs/ReqHandlerModule.java
+++ src/main/java/ca/utoronto/utm/mcs/ReqHandlerModule.java
@@ -2,6 +2,10 @@ package ca.utoronto.utm.mcs;
 
 import dagger.Module;
 import dagger.Provides;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
 
 // Module is an interface used to generate the injector
 
@@ -9,8 +13,17 @@ import dagger.Provides;
 public class ReqHandlerModule {
 
     @Provides
-    public Neo4jDAO provideNeo4jDAO(){
-        return DaggerNeo4jDAOComponent.create().buildNeo4jDAO();
+    public Driver provideDriver() {
+        // credentials given in the handout
+        String username = "neo4j";
+        String password = "123456";
+
+        // This code is used to get the neo4j address, you must use this so that we can mark :)
+        Dotenv dotenv = Dotenv.load();
+        String addr = dotenv.get("NEO4J_ADDR");
+
+        String dbUrl = String.format("bolt://%s:7687", addr);
+        return GraphDatabase.driver(dbUrl, AuthTokens.basic(username, password));
     }
 
 }
